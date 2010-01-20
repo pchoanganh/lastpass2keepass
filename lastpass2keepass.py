f3a90dad315d1263b43456e51af3a4520028f846
Nazarius Kappertaal <nazariuskappertaal@gmail.com>
2010-01-20 12:53:44 +1100

    Added group support.

--- lastpass2keepass.py
+++ lastpass2keepass.py
@@ -7,9 +7,9 @@
 # Example: python lastpassconvertor.py export keepass.db
 #
 # The LastPass format;
-# url,username,password,1extra,name,ignore for now- grouping (\ delimited),last_touch,launch_count,fav
+# url,username,password,1extra,name,ignore for now- grouping(\ delimited),last_touch,launch_count,fav
 
-import sys, csv, time, datetime
+import sys, csv, time, datetime, itertools
 from lxml import etree
 
 fileError = "You either need more permissions or the file does not exist."
@@ -31,7 +31,7 @@ try:
         print "Cannot write to disk... exiting.", fileError
         sys.exit()
 except IOError:
-    print "Cannot read file: '%s'" % (inputFile), fileError
+    print "Cannot read file: '%s'" %(inputFile), fileError
     sys.exit()
 
 
@@ -59,53 +59,67 @@ def keepassxml():
     formattedNow = now.strftime("%Y-%m-%dT%H:%M")
 
     # Initialize tree
-    page = etree.Element ('database')
-    doc = etree.ElementTree (page)
-    headElt = etree.SubElement (page, 'group')
-    title = etree.SubElement (headElt, 'title')
-    title.text = 'LastPassConversion'
-    icon = etree.SubElement (headElt, 'icon')
-    icon.text = '0'
+    page = etree.Element('database')
+    doc = etree.ElementTree(page)
 
     # List of failed entries
     failed = {}
     print lineBreak
-    print "DEBUG of '%s' file conversion to the KeePassXML format, outputing to the '%s' file.\n" % (inputFile,outputFile)
-    
-    # Initilize and loop through all entries
-    for x in allEntries:
-        # Each Entry
-        entryElt = etree.SubElement (headElt, 'entry')
-        # Entry information
+    print "DEBUG of '%s' file conversion to the KeePassXML format, outputing to the '%s' file.\n" %(inputFile,outputFile)
+    resultant = {}
+    for li in allEntries:
         try:
-            etree.SubElement (entryElt, 'title').text = str(x[4])
-            etree.SubElement (entryElt, 'username').text = str(x[1])
-            etree.SubElement (entryElt, 'password').text = str(x[2])
-            etree.SubElement (entryElt, 'url').text = str(x[0])
-            etree.SubElement (entryElt, 'comment').text = str(x[3])
-            etree.SubElement (entryElt, 'icon').text = "0"
-            etree.SubElement (entryElt, 'creation').text = formattedNow
-            etree.SubElement (entryElt, 'lastaccess').text = str(x[6])
-            etree.SubElement (entryElt, 'lastmod').text = str(x[7])
-            etree.SubElement (entryElt, 'expire').text = "Never"
+            resultant.setdefault(li[5], []).append(li)
         except:
-            # Catch illformed entries
-            
+            # Catch illformed entries         
             # Grab entry position
-            p = allEntries.index(x) + 2
-            failed[p] = [",".join(x)]
-            print "Failed to format entry at line %d" % (p)
-            
+            p = allEntries.index(li) + 2
+            failed[p] = [",".join(li)]
+            print "Failed to format entry at line %d" %(p)
+  
+
+    # Initilize and loop through all entries
+    for x, v in resultant.iteritems():
+        headElt = etree.SubElement(page, 'group')
+        etree.SubElement(headElt, 'title').text = str(x)
+        etree.SubElement(headElt, 'icon').text = "0"
+        for entry in v: 
+            # Entry information
+            try:
+                # Each Entry
+                
+                entryElt = etree.SubElement(headElt, 'entry')
+                
+                etree.SubElement(entryElt, 'title').text = str(entry[4])
+                etree.SubElement(entryElt, 'username').text = str(entry[1])
+                etree.SubElement(entryElt, 'password').text = str(entry[2])
+                etree.SubElement(entryElt, 'url').text = str(entry[0])
+                etree.SubElement(entryElt, 'comment').text = str(entry[3])
+                etree.SubElement(entryElt, 'icon').text = "0"
+                etree.SubElement(entryElt, 'creation').text = formattedNow
+                etree.SubElement(entryElt, 'lastaccess').text = str(entry[6])
+                etree.SubElement(entryElt, 'lastmod').text = str(entry[7])
+                etree.SubElement(entryElt, 'expire').text = "Never"
+            except:
+                # Catch illformed entries          
+                # Grab entry position
+                p = allEntries.index(entry) + 2
+                failed[p] = [",".join(entry)]
+                print "Failed to format entry at line %d" %(p)
+                 
     if len(failed) != 0:
-        failedList = ["%d : %s" % (p, str(e[0]).decode("utf-8")) for p, e in failed.items()]
+        failedList = ["%d : %s" %(p, str(e[0]).decode("utf-8")) for p, e in failed.items()]
         print "\nThe conversion was not clean."
-        print "You need to manually import the below entries from the '%s' file, as listed by below." % (inputFile)
+        print "You need to manually import the below entries from the '%s' file, as listed by below." %(inputFile)
         print "Line Number : Entry"
         for x in failedList:
             print x
+
     print lineBreak
+    
     # Write out tree
     doc.write(w)
+    
     return
     
 # Call the keepassxml generator
@@ -115,5 +129,5 @@ keepassxml()
 f.close()
 w.close()           
 
-print "\n'%s' has been succesfully converted to the KeePassXML format." % (inputFile)
-print "Converted data can be found in the '%s' file.\n" % (outputFile)
+print "\n'%s' has been succesfully converted to the KeePassXML format." %(inputFile)
+print "Converted data can be found in the '%s' file.\n" %(outputFile)
