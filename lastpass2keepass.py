176f3ffefbcb24a14d46b662c4f152228bd7a798
anirudhjoshi <github@anirudhjoshi.com>
2011-10-28 11:48:59 +1200

    Fixed parsing multi-line with quotes, decode strings errors.

--- lastpass2keepass.py
+++ lastpass2keepass.py
@@ -5,7 +5,7 @@
 # The LastPass Export format;
 # url,username,password,1extra,name,grouping(\ delimited),last_touch,launch_count,fav
 
-import sys, csv, time, datetime, itertools, re # Toolkit
+import sys, csv, time, datetime, itertools, re, operator # Toolkit
 import xml.etree.ElementTree as ET # Saves data, easier to type
 
 # Strings
@@ -45,7 +45,26 @@ except IOError:
 
 # Parser
 # Parse w/ delimter being comma, and entries separted by newlines
-reader = csv.reader(f, delimiter=',', quotechar='"')
+
+h = re.compile('^http') # Fix multi-line lastpass problems
+q = re.compile(',\d\n')
+
+for line in f.readlines():
+
+	if h.match( line ):
+		w.write( "\n" + line.strip() ) # Each new line is based on this
+	elif q.search( line ):
+		w.write( line.strip() ) # Remove end line
+	else:
+		w.write( line.replace( '\n', '|\t|' ) ) # Place holder for new lines in extra stuff
+
+f.close() # Close the read file.
+
+w.close() # reuse same file - stringIO isn't working
+
+w = open(outputFile, "r") # open for reading - windows problems with reader on stringIO
+
+reader = csv.reader( w, delimiter=',', quotechar='"' ) # use quotechar to fix parsing
 
 # Create a list of the entries, allow us to manipulate it.
 # Can't be done with reader object.
@@ -53,94 +72,86 @@ reader = csv.reader(f, delimiter=',', quotechar='"')
 allEntries = []
 
 for x in reader:
-    allEntries.append(x)
+	allEntries.append(x)
+
+w.close() # reader appears to be lazily evaluated leave - close w here
+
+w = open(outputFile, "w") # clean the file - prepare for xml tree write
 
 allEntries.pop(0) # Remove LP format string.
-
-f.close() # Close the read file.
 
 # Keepass XML generator
    
 # Add doctype to head, clear file.
+
 w.write("<!DOCTYPE KEEPASSX_DATABASE>")
 
 # Generate Creation date
 # Form current time expression.
+
 now = datetime.datetime.now()
 formattedNow = now.strftime("%Y-%m-%dT%H:%M")
 
 # Initialize tree
 # build a tree structure
+
 page = ET.Element('database')
 doc = ET.ElementTree(page)
-
-# Dictionary of failed entries
-failed = {}
-    
-formattedPrint("DEBUG of '%s' file conversion to the KeePassXML format, outputing to the '%s' file." %(inputFile,outputFile))
-    
+  
 # A dictionary, organising the categories.
+
 resultant = {}
     
-# Parses allEntries into a resultant.
+# Parses allEntries into a resultant dict.
+
 for entry in allEntries:
-    try:
-        categories = re.split(r"[/\\]",entry[5]) # Grab final category.
-        
-        for x in categories:
-            resultant.setdefault(categories.pop(), []).append(entry) # Sort by categories.
-    except:
-        # Catch illformed entries         
-        # Grab entryElement position
-        p = allEntries.index(entry) + 2
-        failed[p] = [",".join(entry)]
-        
-        print "Failed to format entryElement at line %s" % (p)
+	resultant.setdefault( entry[5], [] ).append( entry ) 
+
+sorted_resultant = sorted(resultant.iteritems(), key=operator.itemgetter(1)) # sort for xml tree
+
+# Sort by categories.	
 
 # Initilize and loop through all entries
-for category, categoryEntries in resultant.iteritems():
 
+for categoryEntries in sorted_resultant:
+
+	# Place hold sorted data
+	
+    category = categoryEntries[0]
+    entries = categoryEntries[1]
+	
 	# Create head of group elements
+	
     headElement = ET.SubElement(page, "group")
     ET.SubElement(headElement, "title").text = str(category)
     ET.SubElement(headElement, "icon").text = "0" # Lastpass does not retain icons.
     
-    for entry in categoryEntries: 
+    for entry in entries: 
+	
     # entryElement information
-        try:
+
             # Each entryElement
+			
             entryElement = ET.SubElement(headElement, "entry")
+			
             # entryElement tree
-            ET.SubElement(entryElement, 'title').text = str(entry[4]).decode("utf-8")
+			
+            ET.SubElement(entryElement, 'title').text = str(entry[4]).decode("utf-8") # Use decode for windows el appending errors
             ET.SubElement(entryElement, 'username').text = str(entry[1]).decode("utf-8")
             ET.SubElement(entryElement, 'password').text = str(entry[2]).decode("utf-8")
             ET.SubElement(entryElement, 'url').text = str(entry[0]).decode("utf-8")
-            ET.SubElement(entryElement, 'comment').text = str(entry[3]).decode("utf-8")
+            ET.SubElement(entryElement, 'comment').text = str(entry[3]).replace( '|\t|', '\n').strip('"').decode("utf-8") # fix place holder
             ET.SubElement(entryElement, 'icon').text = "0"
             ET.SubElement(entryElement, 'creation').text = formattedNow
             ET.SubElement(entryElement, 'lastaccess').text = str(entry[6]).decode("utf-8")
-            ET.SubElement(entryElement, 'lastmod').text = str(entry[7]).decode("utf-8")
+            ET.SubElement(entryElement, 'lastmod').text = formattedNow
             ET.SubElement(entryElement, 'expire').text = "Never"
-        except:
-            # Catch illformed entries          
-            # Grab entry position
-            p = allEntries.index(entry) + 2
-            failed[p] = [",".join(entry)]
-            print "Failed to format entry at line %d" %(p)
-
-# Check if it was a clean conversion.
-if len(failed) != 0:
-    # Create a failed list.
-    failedList = ["%d : %s" %(p, str(e[0]).decode("utf-8")) for p, e in failed.items()]
-    formattedPrint("The conversion was not clean.")
-    print "You need to manually import the below entries from the '%s' file, as listed by below." %(inputFile)
-    formattedPrint("Line Number : entryElement")
-    for x in failedList:
-        print x
 
 # Write out tree
 # wrap it in an ElementTree instance, and save as XML
+
 doc.write(w)
+
 w.close()
 
 print lineBreak
